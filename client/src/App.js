e28c64c18393eaea44d440408b7c8325d1349d0d
Purna ikkurthi <157563005+purna1031@users.noreply.github.com>
2024-09-04 09:51:33 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import Interact from './pages/intearct';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Home from './pages/home';
 import AboutUs from './pages/aboutus';
+import Job from './pages/jobdiscuss';
+
 const App = () => {
   return (
     <ChakraProvider>
       <Router>
         <Routes>
+          <Route path='/interact' element={<Interact />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path='/jobdiscussion' element={<Job />} />
           <Route path="/" element={<Home />} />
           <Route path="/aboutus" element={<AboutUs/>} />
         </Routes>
